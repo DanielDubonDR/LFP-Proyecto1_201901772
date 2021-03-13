4abbcf53cad656678430047851a995b6aa9b0f0c
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2021-03-13 02:57:06 -0600

    Update Procesar.py

--- Procesar.py
+++ Procesar.py
@@ -1,8 +1,9 @@
+import re
 
 class Analizar:
     def __init__(self, ruta):
        self.ruta=ruta
-       self.Preservada="restaurante"
+       self.contador=1
        self.leerLinea()
 
     def leerLinea(self):
@@ -20,10 +21,63 @@ class Analizar:
     def leerCaracteres(self, linea):
 
         if linea.startswith("r"):
-            cont=1
-            reservada=""
+            columna=1
             estado=0
             string=""
+            limpiar=False
+            for caracter in linea:
+                if estado==0:
+                    if string.rstrip()=="restaurante":
+                        estado=1
+                        asd=str("No = ")+str(self.contador)+str(", Lexema = ")+str(string)+str(", Fila = ")+str(self.contLinea)+str(", Columna = ")+str(columna)+str(", Token = Palabra Reservada")
+                        print(asd)
+                        string=""
+                        self.contador+=1
+
+                    elif caracter=="=":
+                        estado=2
+
+                    elif caracter=="'":
+                        estado=3
+
+                    else:
+                        if re.search(r"[a-z]",caracter):
+                            string+=caracter
+                        else:
+                            string+=caracter
+                            print("caracter desconocido",caracter)
+
+                if estado==1:
+                    if caracter=="=":
+                        columna+=1
+                        asd=str("No = ")+str(self.contador)+str(", Lexema = ")+str(caracter)+str(", Fila = ")+str(self.contLinea)+str(", Columna = ")+str(columna)+str(", Token = Signo Igual")
+                        print(asd)
+                        self.contador+=1
+                        estado=2
+                    elif caracter=="'":
+                        print("ERROR, se esperaba =")
+                        estado=100
+                        #-----------------------------corregir
+                    elif str.isalpha(caracter):
+                        print("ERROR, se esperaba = y '")
+                        estado=100
+                
+                if estado==3:
+                    if caracter=="'":
+                        columna+=1
+                        asd=str("No = ")+str(self.contador)+str(", Lexema = ")+str(caracter)+str(", Fila = ")+str(self.contLinea)+str(", Columna = ")+str(columna)+str(", Token = Signo Igual")
+                        print(asd)
+                        self.contador+=1
+                        estado=2
+                    elif caracter=="'":
+                        print("ERROR, se esperaba =")
+                        estado=100
+                    elif re.search(r"[^]",caracter):
+                        print("ERROR, se esperaba = y '")
+                        estado=100
+                
+                columna+=1
+            '''
             if "=" in linea:
                 for caracter in linea:
                     if estado==0:
@@ -49,6 +103,7 @@ class Analizar:
 
         else:
             self.reservada=False
+        '''
 
         '''
         if linea.startswith("'"):
